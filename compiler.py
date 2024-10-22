04ff85d6d1ede2be81075d324bbbf596a8202f7f
Mohammed Morra <wolfeyplays123@gmail.com>
2024-10-22 21:11:00 +0300

    added things

--- compiler.py
+++ compiler.py
@@ -13,12 +13,11 @@ def isCode(x):
     return x not in [" ","\n",""]
 
 queue=filter(isCode,queue)
-print(queue)
-
 
 # program states
-array=[0]*20
+array=[0]*64
 p=0
+cP=0
 acc=0
 
 for s in queue:
@@ -30,8 +29,40 @@ for s in queue:
         array[p]+=int(s[1:])
     elif s[0]=="-":
         array[p]-=int(s[1:])
-    
-print(array)
+    elif s[0]=="!": # assigning a value to the accumulator
+        if s[2]=="+":
+            acc+=int(s[2:])
+        elif s[2]=="-":
+            acc-=int(s[2:])
+        elif s[2]=="@": # assign the value at the pointer as it is
+            acc=array[p]
+        else:
+            acc+=array[p] # add the value at the pointer to the accumulator
+    elif s[0]=="_": # loop logic
+        cP+=p-cP #moving the control pointer to the current pointer address
+        while array[cP] > 0:
+            for segment in s[1:].split(":"):
+                if segment[0]==">":
+                    p+=int(segment[1:])
+                elif segment[0]=="<":
+                    p-=int(segment[1:])
+                elif s[0]=="+":
+                    array[p]+=int(segment[1:])
+                elif segment[0]=="-":
+                    array[p]-=int(segment[1:])
+                elif segment[0]=="!": # assigning a value to the accumulator
+                    if segment[2]=="+":
+                        acc+=int(segment[2:])
+                    elif segment[2]=="-":
+                        acc-=int(segment[2:])
+                    elif segment[2]=="@": # assign the value at the pointer as it is
+                        acc=array[p]
+                    else:
+                        acc+=array[p] # add the value at the pointer to the accumulator
+            array[cP]-=1
+
+
+print("array:",array)
 for s in array:
     if s>=32 and s<=126:
         print(chr(s),end="")
